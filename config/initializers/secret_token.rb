eeb31856d3a68d2095583495c1fc173af6387a1d
FayHernandez <hernandezfay@gmail.com>
2013-09-29 04:05:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fayresume::Application.config.secret_key_base = 'ac6b58a83271b409c10bda4afbf17cd6c7e5e2de1827b2f6a3a75ba1e79f5d0ccb7aac399273a664d7c4d75eeae4b129b46802ff2d589b5811dc1227f887e139'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fayresume::Application.config.session_store :cookie_store, key: '_fayresume_session'
